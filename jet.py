a45e933c1afccdd4ff7b033b9b202375c318af05
FondTarsier4876 <breslavmich@gmail.com>
2022-10-14 21:50:20 +0300

    Adding comments

--- jet.py
+++ jet.py
@@ -40,10 +40,12 @@ class Jet:
         self.is_white = is_white
 
     def go_forward(self) -> None:
+        """Updating the planes position to move forward"""
         self.x += -self.speed * sin(radians(self.angle))
         self.y += -self.speed * cos(radians(self.angle))
 
     def keep_in_map(self) -> None:
+        """Keeping the jet in the bounds of the screen"""
         width, height = self.image.get_size()
         if self.x >= self.screen_width:
             self.x = -width
@@ -56,6 +58,7 @@ class Jet:
             self.y = self.screen_height
 
     def draw(self, screen: pygame.Surface) -> None:
+        """Drawing the jet on the screen"""
         rotated_image, rect = rot_center(self.image, self.angle)
         rect.x = self.x
         rect.y = self.y
@@ -63,38 +66,48 @@ class Jet:
         self.draw_bullets(screen)
 
     def update(self, enemy_bullets: list, hits_list: list) -> None:
+        """Updating the jet's parameters"""
+        # Updating the position
         self.go_forward()
         self.keep_in_map()
+        # Updating the angle
         if self.rotate_amount > 7:
             self.rotate_amount = 7
         elif self.rotate_amount < -7:
             self.rotate_amount = -7
         self.angle += self.rotate_amount
 
+        # Updating all bullets
         for bullet in self.bullets:
             if bullet.time_alive > 200:
                 self.bullets.remove(bullet)
             else:
                 bullet.update()
-
+        # Checking if plane was hit
         self.check_hits(enemy_bullets, hits_list)
 
     def check_hits(self, enemy_bullets: list, hits_list: list):
+        """Checking if plane was hit"""
         for bullet in enemy_bullets:
-            if math.dist((self.x + self.image.get_width() / 2, self.y + self.image.get_height() / 2), (bullet.x, bullet.y)) < 10 + bullet.radius:
+            if math.dist((self.x + self.image.get_width() / 2, self.y + self.image.get_height() / 2),
+                         (bullet.x, bullet.y)) < 13 + bullet.radius:
+                # Checking if bullet hit the plane
                 hits_list.append(bullet)
                 enemy_bullets.remove(bullet)
 
     def shoot(self) -> None:
+        """Shooting a bullet"""
         bullet = Bullet(self.screen_width, self.screen_height, int(self.x + self.image.get_width() / 2),
-                        int(self.y + self.image.get_height() / 2), self.angle, self.is_white)
-        self.bullets.append(bullet)
+                        int(self.y + self.image.get_height() / 2), self.angle, self.is_white)  # Creating a bullet
+        self.bullets.append(bullet)  # Adding bullet to the list
 
     def draw_bullets(self, screen: pygame.Surface) -> None:
+        """Drawing all the bullets for the plane"""
         for bullet in self.bullets:
             bullet.draw(screen)
 
     def to_dict(self) -> dict:
+        """Returning a description dictionary of the parameters of the plane to send to the client"""
         description = vars(self).copy()
         desc_bullets = []
         try:
@@ -109,6 +122,7 @@ class Jet:
         return description
 
     def new_bullet_from_dict(self, description_dict: dict) -> None:
+        """Creating a new bullet based on parameters from a dictionary"""
         new_bullet = Bullet(screen_width=description_dict['screen_width'],
                             screen_height=description_dict['screen_height'],
                             x=description_dict['x'],
@@ -118,6 +132,7 @@ class Jet:
         self.bullets.append(new_bullet)
 
     def data_from_dict(self, description_dict: dict) -> None:
+        """Updating a jet's parameters based on parameters from a dictionary"""
         self.x = description_dict['x']
         self.y = description_dict['y']
 
